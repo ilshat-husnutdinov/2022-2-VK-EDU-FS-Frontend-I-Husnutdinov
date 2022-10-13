1491b5aafbbdcb078038a122e2b4204cc1c3f500
ilshat husnutdinov <husnutd1nov.ilshat@yandex.ru>
2022-10-14 00:26:02 +0400

    Add 2 homework

---
+++ simple-chat/src/index.js
@@ -0,0 +1,122 @@
+import './index.css';
+
+// Закрыть меню доп. информации в блоке .more при нажатии вне блока .more
+window.addEventListener('click', function(event) {
+if (!event.target.matches('.material-icons')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+    }
+    }
+}
+})
+
+// Автоматический скролл вниз при отправке нового сообщения
+function scroll() {
+        document.querySelector('.message').scrollTop = 
+        document.querySelector('.message').scrollHeight;
+    }
+
+// Модель информации о сообщении для сохранения в LocaleStorage
+class InfoMessage {
+    constructor(message, author, dateTime){
+        this.message = message;     // сообщение   
+        this.author = author;       // автор
+        this.dateTime = dateTime;   // время отправки
+    }
+}
+
+const form = document.querySelector('form'); 
+const input = document.querySelector('.form-input');
+const message = document.querySelector('.message');
+
+const messageStoreKey = 'message_store_key';
+let infoMessages = getInfoMessagesFromLocStore();
+
+refresh(); 
+
+form.addEventListener('submit', handleSubmit.bind(this));
+// form.addEventListener('keypress', this.handleKeyPress.bind(this));
+
+// отправить сообщение
+function handleSubmit (event) {
+    event.preventDefault();
+    if (input.value.trim() === '') {
+        input.value = '';
+        return;
+    }
+
+    const info = new InfoMessage(input.value, 'ilshat', new Date());
+    createBlockMessage(info);
+    scroll();
+    setInfoMessagesToLocStore(info);
+    input.value = '';
+}
+
+// получить сообщения из LocaleStorage
+function getInfoMessagesFromLocStore() {
+    const infoMessagesFromLocalStore = localStorage.getItem(messageStoreKey);
+    return JSON.parse(infoMessagesFromLocalStore);
+}
+
+// сохранить информацию о сообщении в LocaleStorage
+function setInfoMessagesToLocStore(info) {
+    infoMessages.push(info);
+    localStorage.setItem(messageStoreKey,JSON.stringify(infoMessages));
+}
+
+// создать блок сообщения
+function createBlockMessage(info) {
+    let container = document.createElement('div'); 
+    container.className = 'message-container';
+
+    let message_text = document.createElement('div')
+    message_text.className = 'message-text';
+    message_text.innerHTML = info.message;
+
+    let message_time = document.createElement('div');
+    message_time.className = 'message-time';
+    let dateTime = new Date(info.dateTime);
+    let res = [dateTime.getHours(), dateTime.getMinutes()].map(function (x) {
+        return x < 10 ? "0" + x : x
+    }).join(":");
+    message_time.innerHTML = '<p>' + res + '</p>';
+    
+    let message_status = document.createElement('span'); 
+    message_status.className = 'material-icons';
+    message_status.innerHTML = 'done_all'
+
+    message_time.appendChild(message_status);
+    container.appendChild(message_text);
+    container.appendChild(message_time);
+    message.appendChild(container);
+}
+
+// получить сообщения при обновлении
+function refresh() {          
+    
+    if (!infoMessages) {
+        infoMessages = new Array();
+        return;
+    }
+    infoMessages.forEach(x => {
+        createBlockMessage(x);
+    });
+}
+
+// Раскрыть меню доп. информации в блоке .more
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+// }
+// document.querySelector("dropbtn").addEventListener("click", myFunction(this));
+// событие нажатия клавиши Enter
+function handleKeyPress (event) {
+    if (event.keyCode === 13) {
+        form.dispatchEvent(new Event('submit'));
+        console.log('kyepresss')
+    }
+}
