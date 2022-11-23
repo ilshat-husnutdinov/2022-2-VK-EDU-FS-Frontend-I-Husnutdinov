a6d8bbc1671336620eb6e29f5e81ceb4506355f3
ilshat husnutdinov <husnutd1nov.ilshat@yandex.ru>
2022-11-23 14:55:44 +0400

    change message scrolling using useRef

--- react-chat/src/components/ChatComponents/MessageBlock/Messages.jsx
+++ react-chat/src/components/ChatComponents/MessageBlock/Messages.jsx
@@ -1,28 +1,33 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import styles from './Messages.module.css';
 import { Message } from '../../../components/Message';
-import { scroll } from '../../../utils';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 export function Messages(props) {
+    const  messagesEndRef = useRef(null);
 
     useEffect(() => {
-		scroll(styles.message);
+		messagesEndRef.current.scrollIntoView({behavior: 'smooth'});
     },
     [props.chatMessages]);
 
     return (
-            <div className={styles.message}>
+            <div className={styles.message} >
                 <TransitionGroup>
                     {props.chatMessages.map(chatMsg =>
-                        <CSSTransition key={chatMsg.id} timeout={300} classNames={{
-                            enter: styles.msg_enter,
-                            enterActive: styles.msg_enter_active,
-                        }}>
+                        <CSSTransition
+                            key={chatMsg.id}
+                            timeout={300}
+                            classNames={{
+                                enter: styles.msg_enter,
+                                enterActive: styles.msg_enter_active,
+                            }}
+                        >
                             <Message text={chatMsg.text} time={chatMsg.time}/>
                         </CSSTransition>
                     )}
                 </TransitionGroup>
+                <div ref={messagesEndRef}></div>
             </div>
     )
 }
